72de01423dd2d3d534b265fb42e3d8f12c596247
Rasmus Eneman <pie.or.paj@gmail.com>
2014-06-09 22:45:29 +0200

    Better comment

---
+++ lib/plugin.dart
@@ -0,0 +1,6 @@
+library raxa.plugin;
+
+import 'dart:async';
+import 'package:raxa/configuration.dart';
+
+part 'src/plugin/manager.dart';
--- lib/src/configuration/database.dart
+++ lib/src/configuration/database.dart
@@ -1,7 +1,8 @@
 part of raxa.configuration;
 
 /**
- * A wrapper for mongo_darts [Db]
+ * A wrapper for mongo_darts [Db] that connects to the database
+ * as specified in the [Config].
  */
 class Database extends Db {
     Database(Config config) : super(config.dbString);
