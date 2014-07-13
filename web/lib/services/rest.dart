d7f5a70953dd904c218a30d30d984fdb6982b25a
Rasmus Eneman <pie.or.paj@gmail.com>
2014-07-13 18:59:08 +0200

    Style fixes

--- web/lib/services/rest.dart
+++ web/lib/services/rest.dart
@@ -6,39 +6,27 @@ class RestService {
 
     RestService(this.http);
 
-    Future call(Call call) {
-        return http.post('http://127.0.0.1:8080/rest/call', JSON.encode(call));
-    }
+    _query(Map query) =>
+        (query != null) ? '?query=${Uri.encodeQueryComponent(JSON.encode(query))}' : '';
 
-    Future<List<Device>> getDevices([Map query]) {
-        var queryParam = '';
+    Future call(Call call) =>
+        http.post('http://127.0.0.1:8080/rest/call', JSON.encode(call));
 
-        if (query != null) {
-            queryParam = '?query=${Uri.encodeQueryComponent(JSON.encode(query))}';
-        }
-
-        return http.get('http://127.0.0.1:8080/rest/devices$queryParam').then((response) {
-            return response.data['data'].map((json) => new Device.from(json)).toList();
-        });
-    }
-
-    Future<List<DeviceClass>> getDeviceClasses([Map query]) {
-        var queryParam = '';
-
-        if (query != null) {
-            queryParam = '?query=${Uri.encodeQueryComponent(JSON.encode(query))}';
-        }
-
-        return http.get('http://127.0.0.1:8080/rest/deviceclasses$queryParam').then((response) {
+    Future<List<DeviceClass>> getDeviceClasses([Map query]) =>
+        http.get('http://127.0.0.1:8080/rest/deviceclasses${_query(query)}').then((response) {
             return response.data['data'].map((json) => new DeviceClass.from(json)).toList();
         });
-    }
 
     Future createDevice(Device device) =>
         http.post('http://127.0.0.1:8080/rest/devices', JSON.encode(device));
 
     Future deleteDevice(Device device) =>
         http.delete('http://127.0.0.1:8080/rest/devices/${device.id}');
+
+    Future<List<Device>> getDevices([Map query]) =>
+        http.get('http://127.0.0.1:8080/rest/devices${_query(query)}').then((response) {
+            return response.data['data'].map((json) => new Device.from(json)).toList();
+        });
 
     Future saveDevice(Device device) =>
         http.put('http://127.0.0.1:8080/rest/devices/${device.id}', JSON.encode(device));
