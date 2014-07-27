f5ccfead6d0b2fb73890a04a8eec29a6ca51fa3b
Rasmus Eneman <pie.or.paj@gmail.com>
2014-07-27 15:20:02 +0200

    (Plugin/Scenarios)  Add A Scenario plugin

---
+++ bin/plugins/Scenarios/main.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+import 'package:raxa/plugin_helper.dart';
+
+main(args, message) {
+    var plugin = new Plugin(args, message)
+        ..onCalled.listen((message) {
+            if (message.call.interface == 'Scenario' && message.call.method == 'set') {
+                message.device.config['scenario']['value'].forEach((call) =>
+                    http.post('http://127.0.0.1:8080/rest/call', body: JSON.encode(call['value'])));
+            }
+        });
+}
