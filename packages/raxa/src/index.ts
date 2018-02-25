def70f474fffdc8932d8420fc386b0abd2a188ac
Rasmus Eneman <rasmus@eneman.eu>
2018-02-25 17:43:17 +0100

    chore: Add bugsnag reporting

--- packages/raxa/src/index.ts
+++ packages/raxa/src/index.ts
@@ -1,12 +1,30 @@
 import 'babel-polyfill'
+import bugsnag from 'bugsnag'
 import {main} from './lib/server/app'
+
+if (process.env.BUGSNAG_API_KEY !== undefined) {
+  const metaData = process.env.INSTALLATION
+    ? {user: {installation: process.env.INSTALLATION}}
+    : undefined
+
+  bugsnag.register(process.env.BUGSNAG_API_KEY, {
+    appVersion: process.env.APP_VERSION,
+    metaData,
+  })
+}
 
 if (process.env.NODE_ENV !== 'production') {
   require('source-map-support/register')
 }
 
 process.on('unhandledRejection', (reason, p) => {
-  console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason, reason && reason.stack)
+  console.error(
+    'Unhandled Rejection at: Promise ',
+    p,
+    ' reason: ',
+    reason,
+    reason && reason.stack,
+  )
 })
 
 main()
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,6 +1,15 @@
+import bugsnag from 'bugsnag-js'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {App} from './app'
 import './index.css'
+
+if (process.env.REACT_APP_BUGSNAG_API_KEY !== undefined) {
+  const client = bugsnag(process.env.REACT_APP_BUGSNAG_API_KEY)
+  ;(client.app as any).version = process.env.REACT_APP_VERSION
+  client.metaData = (process.env.REACT_APP_INSTALLATION
+    ? {user: {installation: process.env.REACT_APP_INSTALLATION}}
+    : undefined) as any
+}
 
 ReactDOM.render(<App />, document.getElementById('root'))
