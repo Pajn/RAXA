527d4d3bfb70808306804aa8ada4ff816d26fdc8
Rasmus Eneman <rasmus@eneman.eu>
2018-02-25 16:50:07 +0100

    chore: Add arm docker cross build

--- build.sh
+++ build.sh
@@ -1,2 +1,9 @@
-node dockerfile.js > Dockerfile.gen
-docker build . -f Dockerfile.gen -t raxa
+#!/bin/bash
+
+if [ "$1" = "--arm" ]; then
+  node dockerfile.js --arm > Dockerfile.gen.arm
+  docker build . -f Dockerfile.gen.arm -t raxa:arm
+else
+  node dockerfile.js > Dockerfile.gen
+  docker build . -f Dockerfile.gen -t raxa
+fi
--- dockerfile.js
+++ dockerfile.js
@@ -1,18 +1,34 @@
-const globby = require('globby');
+const fs = require('fs')
+const globby = require('globby')
+
+const isArm = process.argv.includes('--arm')
+
+if (isArm) {
+  if (!fs.existsSync('qemu-arm-static')) {
+    fs.copyFileSync('/usr/bin/qemu-arm-static', 'qemu-arm-static')
+  }
+}
+
+const baseImage = isArm ? 'arm32v7/node:8.9-slim' : 'node:8.9-alpine'
+
+const setup = isArm ? 'COPY qemu-arm-static /usr/bin/qemu-arm-static' : ''
 
 console.log(`
-FROM node:8-alpine as build
+FROM ${baseImage} as build
 WORKDIR /app
+
 
 COPY package.json .
 COPY .yarnrc .
 COPY yarn.lock .
 COPY lerna.json .
 COPY install.sh .
-${globby.sync(['packages/*/{package.json,yarn.lock}']).map(path =>
-`COPY ${path} /app/${path}`
-).join('\n')}
+${globby
+  .sync(['packages/*/{package.json,yarn.lock}'])
+  .map(path => `COPY ${path} /app/${path}`)
+  .join('\n')}
 
+${setup}
 RUN ./install.sh && yarn cache clean
 
 COPY packages /app/packages
@@ -24,7 +40,7 @@ ENV RAXA_DATA_DIR=/config
 RUN ./run.sh yarn build
 RUN ./run.sh rm -rf node_modules
 
-FROM node:8-alpine
+FROM ${baseImage}
 WORKDIR /app
 
 EXPOSE 8000
@@ -35,11 +51,13 @@ COPY .yarnrc .
 COPY yarn.lock .
 COPY lerna.json .
 COPY install.sh .
-${globby.sync(['packages/*/{package.json,yarn.lock}']).map(path =>
-`COPY ${path} /app/${path}`
-).join('\n')}
+${globby
+  .sync(['packages/*/{package.json,yarn.lock}'])
+  .map(path => `COPY ${path} /app/${path}`)
+  .join('\n')}
 
-RUN SKIP_WEB=1 ./install.sh --production && yarn cache clean
+${setup}
+RUN SKIP_WEB=1 ./install.sh --concurrency=1 --production && yarn cache clean
 
 COPY --from=build /app /app/
 
