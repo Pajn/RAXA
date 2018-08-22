66208502d57a29eeebf0b4baf44f4d848597ae97
Rasmus Eneman <rse@learningwell.se>
2018-08-22 23:02:47 +0200

    perf(web): Delay bugsnag loading

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -3,24 +3,46 @@ import ReactDOM from 'react-dom'
 import {App} from './app'
 import './index.css'
 
-if (process.env.REACT_APP_BUGSNAG_API_KEY !== undefined) {
-  const s = document.createElement('script')
-  s.async = true
-  s.setAttribute(
-    'src',
-    'https://d2wy8f7a9ursnm.cloudfront.net/v4/bugsnag.min.js',
-  )
-  s.onload = () => {
-    const bugsnagClient = (window as any).bugsnag(
-      process.env.REACT_APP_BUGSNAG_API_KEY,
-    )
-    bugsnagClient.app.version = process.env.REACT_APP_VERSION
-    bugsnagClient.metaData = process.env.REACT_APP_INSTALLATION
-      ? {user: {installation: process.env.REACT_APP_INSTALLATION}}
-      : undefined
-    ;(window as any).bugsnagClient = bugsnagClient
+type RequestIdleCallbackHandle = any
+type RequestIdleCallbackOptions = {
+  timeout: number
+}
+type RequestIdleCallbackDeadline = {
+  readonly didTimeout: boolean
+  timeRemaining: (() => number)
+}
+
+declare global {
+  interface Window {
+    requestIdleCallback?: ((
+      callback: ((deadline: RequestIdleCallbackDeadline) => void),
+      opts?: RequestIdleCallbackOptions,
+    ) => RequestIdleCallbackHandle)
+    cancelIdleCallback: ((handle: RequestIdleCallbackHandle) => void)
   }
-  document.body.appendChild(s)
+}
+
+if (process.env.REACT_APP_BUGSNAG_API_KEY !== undefined) {
+  const delay = window.requestIdleCallback || setTimeout
+  delay(() => {
+    const s = document.createElement('script')
+    s.async = true
+    s.setAttribute(
+      'src',
+      'https://d2wy8f7a9ursnm.cloudfront.net/v4/bugsnag.min.js',
+    )
+    s.onload = () => {
+      const bugsnagClient = (window as any).bugsnag(
+        process.env.REACT_APP_BUGSNAG_API_KEY,
+      )
+      bugsnagClient.app.version = process.env.REACT_APP_VERSION
+      bugsnagClient.metaData = process.env.REACT_APP_INSTALLATION
+        ? {user: {installation: process.env.REACT_APP_INSTALLATION}}
+        : undefined
+      ;(window as any).bugsnagClient = bugsnagClient
+    }
+    document.body.appendChild(s)
+  })
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
