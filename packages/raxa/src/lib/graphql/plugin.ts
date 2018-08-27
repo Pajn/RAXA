554a50185709d17358010392120c371c1ae00fb6
Rasmus Eneman <rse@learningwell.se>
2018-08-27 20:26:33 +0200

    chore(raxa): Allow all plugins to be upgraded in dev

--- packages/raxa/src/lib/graphql/plugin.ts
+++ packages/raxa/src/lib/graphql/plugin.ts
@@ -57,7 +57,8 @@ export const pluginQueries = buildQueries({
           upgradable:
             installedPlugins[plugin.id] &&
             installedPlugins[plugin.id].version &&
-            semver.gt(plugin.version, installedPlugins[plugin.id].version),
+            (semver.gt(plugin.version, installedPlugins[plugin.id].version) ||
+              process.env.NODE_ENV === 'development'),
         }))
         .sort((a, b) => a.name.localeCompare(b.name))
       return plugins
