502b1fa885b5a0c42ba71c785ff0ed43a7d07b89
Rasmus Eneman <rse@learningwell.se>
2018-08-28 19:45:44 +0200

    chore(CI): Fix build.sh for CI

--- build.sh
+++ build.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
 
-export GIT_HASH=`git show HEAD --pretty=format:"%h" --no-patch`
 export BUILD_DATE=`date +'%C%y-%m-%d'`
 
+if [ $CI_COMMIT_SHA ]; then
+  export GIT_HASH="$CI_COMMIT_SHA"
+else
+  export GIT_HASH=`git show HEAD --pretty=format:"%h" --no-patch`
+fi
+
 if [ "$1" = "--arm" ]; then
-  node dockerfile.js --arm > Dockerfile.gen.arm
+  docker run -e BUILD_DATE="$BUILD_DATE" -e GIT_HASH="$GIT_HASH" -v "$PWD":/app -w /app node:alpine node --arm dockerfile.js > Dockerfile.gen.arm
   docker build . -f Dockerfile.gen.arm -t raxa:arm
 else
-  node dockerfile.js > Dockerfile.gen
+  docker run -e BUILD_DATE="$BUILD_DATE" -e GIT_HASH="$GIT_HASH" -v "$PWD":/app -w /app node:alpine node dockerfile.js > Dockerfile.gen
   docker build . -f Dockerfile.gen -t raxa
 fi
